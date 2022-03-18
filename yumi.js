a3bd512aa463ef62a63c7209269c4bd2220534b7
jym0415 <jym9504@gmail.com>
2022-03-18 17:02:17 +0900

    라우팅완료

--- yumi.js
+++ yumi.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+var getsend = require('./routes/getsend')
+var postsend = require('./routes/postsend')
+var sqlsend = require('./routes/sqlsend')
 
 app.get('/',(req, res) => {
     res.send('첫번째 페이지셋팅');
 })
+
+app.use('/get', getsend)
+app.use('/post', postsend)
+app.use('/sqlt', sqlsend)
 
 app.listen(8080,() => {
     console.log('구동완료!')
