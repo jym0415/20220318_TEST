9fec912e6e1cc1e2c2d451ab1fb1ba64899c16a6
jym0415 <jym9504@gmail.com>
2022-03-18 17:29:49 +0900

    리액트DB연동

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,8 +1,21 @@
 var express = require('express')
 router = express.Router()
 
+var mysql = require('mysql');
+var dbconfig = require('../db/config.js')
+const connection = mysql.createConnection(dbconfig)
+
 router.get('/',(req,res) => {
     res.send('get에서옴')
 })
+
+router.get('/awssql',(req, res) => {
+    connection.query('SELECT * FROM reactinterview.interview', (error, rows) => {
+        if(error) throw error;
+        console.log('해당스키마안의 테이블 내용 : ', rows);
+        res.send(rows);
+    });
+})
+
 
 module.exports = router;
